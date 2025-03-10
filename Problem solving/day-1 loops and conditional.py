469c07433d5c70eae2f6f5baeecc832f23d3d8e0
Abhinavsai-12 <abhinavsai1252@gmail.com>
2025-03-10 23:22:48 +0530

    ps day-3

--- Problem solving/day-1 loops and conditional.py
+++ Problem solving/day-1 loops and conditional.py
@@ -1,91 +1,91 @@
-# intro loops and conditional statements
+# # intro loops and conditional statements
 
 
-# 1. elif program
-n=int(input("Enter A number"))
-# n=10
-if n>10:
-    print(n," is greater than 10")
-elif n<10:
-    print(n," is less than 10")
-else:
-    print("Not A Number")
+# # 1. elif program
+# n=int(input("Enter A number"))
+# # n=10
+# if n>10:
+#     print(n," is greater than 10")
+# elif n<10:
+#     print(n," is less than 10")
+# else:
+#     print("Not A Number")
 
 
-# # 2. even or odd
-n=15
-if n%2==0:
-    print(n, "is even")
-else:
-    print(n,"is even")
-
-
-
-# 3. prime numbers
-n=11
-c=0
-for x in range(2,n-1,1):
-    if n%x==0:
-        c+=1
-
-if c==0:
-    print(n,"is a prime")
-else:
-    print("not a prime")
+# # # 2. even or odd
+# n=15
+# if n%2==0:
+#     print(n, "is even")
+# else:
+#     print(n,"is even")
 
 
 
-# 4. sum of individual numbers
+# # 3. prime numbers
+# n=11
+# c=0
+# for x in range(2,n-1,1):
+#     if n%x==0:
+#         c+=1
 
-n="1234"
-sum=0
-for i in n:
-    sum+=int(i)
-print(sum)
-
-
-
-
-# 5. sum of odd individual numbers present in given number
-n="237"
-sum=0
-for i in n:
-    if int(i)%2 !=0:
-        sum+=int(i)
-
-print(sum)
+# if c==0:
+#     print(n,"is a prime")
+# else:
+#     print("not a prime")
 
 
-# 6. difference b/w even and odd
-n="2376"
-esum=0
-osum=0
-for i in n:
-    if int(i)%2 ==0:
-        esum+=int(i)
-    else:
-        osum+=int(i)
 
-if esum >osum:
-    print(esum-osum)
-else:
-    print(osum-esum)
+# # 4. sum of individual numbers
+
+# n="1234"
+# sum=0
+# for i in n:
+#     sum+=int(i)
+# print(sum)
 
 
 
 
+# # 5. sum of odd individual numbers present in given number
+# n="237"
+# sum=0
+# for i in n:
+#     if int(i)%2 !=0:
+#         sum+=int(i)
 
-# 7. which is largest sum even or odd 
-n="2376"
-esum=0
-osum=0
-for i in n:
-    if int(i)%2 ==0:
-        esum+=int(i)
-    else:
-        osum+=int(i)
+# print(sum)
 
-if esum >osum:
-    print(esum,"even sum is greater")
-else:
-    print(osum,"odd sum is greater")
+
+# # 6. difference b/w even and odd
+# n="2376"
+# esum=0
+# osum=0
+# for i in n:
+#     if int(i)%2 ==0:
+#         esum+=int(i)
+#     else:
+#         osum+=int(i)
+
+# if esum >osum:
+#     print(esum-osum)
+# else:
+#     print(osum-esum)
+
+
+
+
+
+# # 7. which is largest sum even or odd 
+# n="2376"
+# esum=0
+# osum=0
+# for i in n:
+#     if int(i)%2 ==0:
+#         esum+=int(i)
+#     else:
+#         osum+=int(i)
+
+# if esum >osum:
+#     print(esum,"even sum is greater")
+# else:
+#     print(osum,"odd sum is greater")
