80a377f64a59d75cdc1202c882dd3e8d367a6e27
Abhinavsai-12 <abhinavsai1252@gmail.com>
2025-04-05 03:01:55 +0530

    os_module in python

---
+++ Day-20 os module/os_practice.py
@@ -0,0 +1,18 @@
+import os
+
+print(os.getcwd())  # Output: Current directory path
+
+# #os.chdir("/path/to/directory")  # Change working directory
+# print(os.getcwd())  # Verify the change
+
+# print(os.listdir())  # Lists files & folders in current directory
+# #print(os.listdir("/path/to/directory"))  # List files in a specific directory
+
+# os.mkdir("new_folder")  # Creates a folder
+# # Create multiple directories (Nested Folders)
+# os.makedirs("parent_folder/child_folder")  
+
+
+
+
+
